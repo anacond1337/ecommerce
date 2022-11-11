ce3be19d9f44e67f8d77bb45f1f25abfb7734733
anacond1337 <andrasarmand0419@gmail.com>
2022-11-11 15:08:17 +0100

    feat: added Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import {
   Route,
   Routes,
 } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Home from './pages/Home';
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cart" />
